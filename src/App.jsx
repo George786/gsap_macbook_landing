e8c93d85631eb1c91112f032a776d84992e9ba93
George786 <hndsmby123@gmail.com>
2025-10-17 22:03:56 +0530

    Update components and constants; add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import NavBar from "./components/NavBar.js";
+import Hero from './components/Hero.jsx';
 
 const App = () => {
     return (
-        <h1 class="text-3xl font-bold underline text-indigo-700">
-            Hello world!
-        </h1>
+       <main>
+           <NavBar />
+          <Hero />
+       </main>
     )
 }
 export default App
