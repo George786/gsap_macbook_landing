de8e04fef106a1ad3af356ad2b6bb5217682896d
Subhash <subhash@Subhashs-MacBook-Air.local>
2025-10-24 20:56:27 +0530

    Update components, store, and model-related files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from "./components/NavBar.js";
 import Hero from './components/Hero.jsx';
+import {ProductViewer} from "./components/ProductViewer.jsx";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
        <main>
            <NavBar />
           <Hero />
+           <ProductViewer />
        </main>
     )
 }
